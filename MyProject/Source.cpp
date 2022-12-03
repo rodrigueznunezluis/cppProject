0d31d74f81f897545b8f3fa70c4f0b37f903719c
Luis Rodriguez <rodrigueznunezluis19@stud.ase.ro>
2022-12-04 00:37:57 +0200

    i have changed the code i've read the description better and firstly started with a private class

--- MyProject/Source.cpp
+++ MyProject/Source.cpp
@@ -5,30 +5,20 @@
 using std::string;
 using namespace std;
 
-//Class definition for ticket 
 class ticket {
-     public: 
-         char name[10];
-         char cno[10];
-}  t ; //object definition for the ticket
-
-//Class definition for card 
-class card : public ticket { //inheritence for public ticket
+private:
+	static string issuedBy;
+	static int ids;
+	const int ticketid;
+	float price;
+	char* eventname = nullptr;
+	int seatNumber;
+	int row;
+	string stand;
 public:
-    char address[50];
-    char emailid[20];
-} v; //object definition for card
 
-//Prototype Call for the functions definitions.
-void pay(int);
-void random();
-void card();
+	ticket() :ticketid(ids) { ids++; price = 0; }
 
-//Main function 
-int main() {
-    system("CLS");
-    //Integer Declaration
-    int ent, a, b, N, x, cardid;
 
-    char ans;
+
 };

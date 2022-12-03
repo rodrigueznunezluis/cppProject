0e814bf4953d48fcac6965623b83b13172463922
Luis Rodriguez <rodrigueznunezluis19@stud.ase.ro>
2022-12-03 23:54:18 +0200

    Prototype call the functions definitions  ,main functions integer declaration

--- MyProject/Source.cpp
+++ MyProject/Source.cpp
@@ -11,3 +11,24 @@ class ticket {
          char name[10];
          char cno[10];
 }  t ; //object definition for the ticket
+
+//Class definition for card 
+class card : public ticket { //inheritence for public ticket
+public:
+    char address[50];
+    char emailid[20];
+} v; //object definition for card
+
+//Prototype Call for the functions definitions.
+void pay(int);
+void random();
+void card();
+
+//Main function 
+int main() {
+    system("CLS");
+    //Integer Declaration
+    int ent, a, b, N, x, cardid;
+
+    char ans;
+};

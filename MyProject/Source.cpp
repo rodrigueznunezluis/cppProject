861f37da6ce1586d8ecbfbd2830e06b16a130ad0
Luis Rodriguez <rodrigueznunezluis19@stud.ase.ro>
2022-12-03 23:17:23 +0200

    Class and object definition first public class

--- MyProject/Source.cpp
+++ MyProject/Source.cpp
@@ -4,3 +4,10 @@
 #include <string>
 using std::string;
 using namespace std;
+
+//Class definition for ticket 
+class ticket {
+     public: 
+         char name[10];
+         char cno[10];
+}  t ; //object definition for the ticket
